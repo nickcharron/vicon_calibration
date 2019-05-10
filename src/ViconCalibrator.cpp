abd05548ebf9be424b15a271954fd8f23a487b25
nickcharron <nicholas.c.charron@gmail.com>
2019-05-10 19:07:49 -0400

    adding json loader

--- src/ViconCalibrator.cpp
+++ src/ViconCalibrator.cpp
@@ -3,7 +3,11 @@
 #include "vicon_calibration/LidarCylExtractor.h"
 #include <string>
 
-std::string getJSONFileName(std::string file_name) {
+std::string bage_file, initial_calibration;
+std::vector<std::string> image_topics, image_frames, intrinsics, lidar_topics,
+                    lidar_frames;
+
+std::string GetJSONFileName(std::string file_name) {
   std::string file_location = __FILE__;
   file_location.erase(file_location.end() - 23, file_location.end());
   file_location += "data/";
@@ -11,10 +15,39 @@ std::string getJSONFileName(std::string file_name) {
   return file_location;
 }
 
+void LoadJson(std::string file_name){
+  nlohmann::json J;
+  std::ifstream file(file_name);
+  file >> J;
+
+  bage_file = J["bage_file"];
+  initial_calibration = J["initial_calibration"];
+
+  for (const auto &topic : J["image_topics"]) {
+    image_topics.push_back(topic.get<std::string>());
+  }
+
+  for (const auto &topic : J["image_frames"]) {
+    image_frames.push_back(topic.get<std::string>());
+  }
+
+  for (const auto &topic : J["intrinsics"]) {
+    intrinsics.push_back(topic.get<std::string>());
+  }
+
+  for (const auto &topic : J["lidar_topics"]) {
+    lidar_topics.push_back(topic.get<std::string>());
+  }
+
+  for (const auto &topic : J["lidar_frames"]) {
+    lidar_frames.push_back(topic.get<std::string>());
+  }
+
+}
+
 int main() {
-
   std::string config_file;
-  config_file = getJSONFileName("ViconCalibrationConfig.json");
-
+  config_file = GetJSONFileName("ViconCalibrationConfig.json");
+  LoadJson(config_file);
   return 0;
 }
